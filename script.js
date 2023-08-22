ac7c76b95d1b4aead9855b7e629ce784cefca0a0
Michael <michaelnunezt@hotmail.com>
2023-08-21 22:04:54 -0500

    Se adiciono la capacidad de poder validar si el texto tiene caracteres diferentes al abecedario, numero, comas, puntos y espacios en blanco. Si tiene, el usuario sera alertado.

--- script.js
+++ script.js
@@ -1,62 +1,91 @@
-function encriptar() {
+function validarTexto() {
   var input = document.getElementById('text-input').value;
-  var resultadoArea = document.getElementById('output-text');
-  var vacio = document.getElementById('noinput');
-  var textoEncriptado = "";
+  var textoValidado = quitarTildes(input);
 
-  if (input.trim() === '') {
-    resultadoArea.style.display = 'none';
-    return;
+  if (/[^a-zA-Z0-9,.\s]/.test(textoValidado)) {
+    alert("Solo se aceptan letras sin tildes y números.")
+    return false;
+  } else {
+    document.getElementById('text-input').value = textoValidado;
+    return true;
   }
-  
-  // Esta manera de encriptar resulto ser mas facil de implementar que el usado en desencriptar.
-  for (var i = 0; i < input.length; i++) {
-    var letra = input[i].toLowerCase();
-    switch (letra) {
-      case "e":
-        textoEncriptado += "enter";
-        break;
-      case "i":
-        textoEncriptado += "imes";
-        break;
-      case "a":
-        textoEncriptado += "ai";
-        break;
-      case "o":
-        textoEncriptado += "ober";
-        break;
-      case "u":
-        textoEncriptado += "ufat";
-        break;
-      default:
-        textoEncriptado += input[i];
-        break;
+}
+
+function quitarTildes(texto) {
+  return texto
+    .replace(/[áÁ]/g, 'a')
+    .replace(/[éÉ]/g, 'e')
+    .replace(/[íÍ]/g, 'i')
+    .replace(/[óÓ]/g, 'o')
+    .replace(/[úÚüÜ]/g, 'u');
+}
+
+function encriptar() {
+  if (validarTexto() == true) {
+    var input = document.getElementById('text-input').value;
+    var resultadoArea = document.getElementById('output-text');
+    var vacio = document.getElementById('noinput');
+    var textoEncriptado = "";
+
+    if (input.trim() === '') {
+      resultadoArea.style.display = 'none';
+      return;
     }
+    // Esta manera de encriptar resulto ser mas facil de implementar que el usado en desencriptar.
+    for (var i = 0; i < input.length; i++) {
+      var letra = input[i].toLowerCase();
+      switch (letra) {
+        case "e":
+          textoEncriptado += "enter";
+          break;
+        case "i":
+          textoEncriptado += "imes";
+          break;
+        case "a":
+          textoEncriptado += "ai";
+          break;
+        case "o":
+          textoEncriptado += "ober";
+          break;
+        case "u":
+          textoEncriptado += "ufat";
+          break;
+        default:
+          textoEncriptado += input[i];
+          break;
+      }
+    }
+    resultadoArea.textContent = textoEncriptado;
+    resultadoArea.style.display = 'block';
+    vacio.style.display = 'none';
+    console.log("Hecho");
+    activeCopy();
+  } else {
+    console.log("Texto invalido");
   }
-  resultadoArea.textContent = textoEncriptado;
-  resultadoArea.style.display = 'block';
-  vacio.style.display = 'none';
-  console.log("Hecho");
-  activeCopy();
 }
 
 function desencriptar() {
-  var input = document.getElementById('text-input').value;  
-  var resultadoArea = document.getElementById('output-text');
-  var vacio = document.getElementById('noinput');
-  var textoEncriptado = "";
+  if (validarTexto() == true) {
+    var input = document.getElementById('text-input').value;
+    var resultadoArea = document.getElementById('output-text');
+    var vacio = document.getElementById('noinput');
+    var textoEncriptado = "";
 
-  textoEncriptado = input.replace(/enter/g, "e")
-  .replace(/ai/g, "a")
-  .replace(/imes/g, "i")
-  .replace(/ober/g, "o")
-  .replace(/ufat/g, "u"); 
+    textoEncriptado = input.replace(/enter/g, "e")
+      .replace(/ai/g, "a")
+      .replace(/imes/g, "i")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
 
-  resultadoArea.textContent = textoEncriptado;
-  resultadoArea.style.display = 'block';
-  vacio.style.display = 'none';
-  console.log("Hecho");
-  activeCopy();
+    resultadoArea.textContent = textoEncriptado;
+    resultadoArea.style.display = 'block';
+    vacio.style.display = 'none';
+    console.log("Hecho");
+    activeCopy();
+  } else {
+    console.log("Texto invalido");
+  }
 }
 
 
